3ca6c8d73b7bfc8a7f213580f4c70cc1f5779512
Vinicius11s <131825765+Vinicius11s@users.noreply.github.com>
2024-10-09 15:40:09 -0300

    Correção do código Exercício 6

--- Aula_Trigger_02-10/Lista Trigger/Exercicio 6.sql
+++ Aula_Trigger_02-10/Lista Trigger/Exercicio 6.sql
@@ -1,4 +1,4 @@
-/*Crie um trigger para adicionar a palavra “Sr(a)” ao nome de pessoas que nasceram há mais
+/*Crie um trigger para adicionar a palavra â€œSr(a)â€ ao nome de pessoas que nasceram hÃ¡ mais
 de 35 anos */
 
 ALTER TRIGGER trg_adcNome_35
@@ -6,14 +6,19 @@ ON CLIENTE
 FOR INSERT, UPDATE
 AS
 BEGIN
-	UPDATE CLIENTE
-		SET nome = 
-			CASE 
-				WHEN DATEDIFF(YEAR, datanascimento, GETDATE()) > 35 THEN 
-					'Sr(a)' + nome 
-			END
-	END;
+	UPDATE C
+	SET nome = 
+		CASE 
+			WHEN DATEDIFF(YEAR, I.datanascimento, GETDATE()) > 35 THEN 
+				'Sr(a) ' + I.nome
+			ELSE 
+				I.nome
+		END
+	FROM CLIENTE C
+	JOIN INSERTED I ON C.id = I.id;
+END;
 
+INSERT INTO CLIENTE (id, nome, datanascimento, cpf)
+VALUES (8, 'Jurandir', '1800-12-05', '46134160865');
 
-	insert CLIENTE VALUES(8, 'Jurandir', '1800-12-05','46134160865')
-	select * from CLIENTE
+SELECT * FROM CLIENTE;
